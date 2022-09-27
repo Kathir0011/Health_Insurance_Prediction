15d02606d4e1f78330cd12a0ae26c09fe10cc134
Kathir <91379134+Kathir0011@users.noreply.github.com>
2022-09-27 19:46:36 +0530

    Update Frontend.py

--- Frontend.py
+++ Frontend.py
@@ -46,7 +46,7 @@ def main():
     try:
         # getting age from the user
         age = float(st.text_input('Age')+".0")
-        if age < 0 or age > 150:
+        if age < 18 or age > 64:
             st.warning("Enter a Valid Age",icon="❌")
     except:
         pass
