5b7e36899da4e92f750e3ca9414365b00c2488bb
Kathir <91379134+Kathir0011@users.noreply.github.com>
2023-02-02 22:54:03 +0530

    Changed prediction value from absolute to range

--- Frontend.py
+++ Frontend.py
@@ -124,7 +124,9 @@ def main():
             if validated:
                 processed_data = convert_into_dataframe([age, gender, bmi, children, smoker])
                 result = make_prediction(processed_data)
-                st.success(f"Predicted Cost: ${result:.0f}")
+                min_res = result - 2500
+                max_res = result + 2500
+                st.success(f"Predicted Range: \${min_res:.0f} to \${max_res:.0f}")
 
             else:
                 st.error('Invalid Input. Try again!!!', icon="⚠️")
